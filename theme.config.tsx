a9c9c9c95e7b42e44df112d690e186c114131440
Ankit kushwaha <119472022+Ankitkushwaha90@users.noreply.github.com>
2024-05-03 04:33:45 -0400

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -37,7 +37,7 @@ const config: DocsThemeConfig = {
   },
   docsRepositoryBase: 'https://github.com/ankitkushwaha90',
   footer: {
-    text: 'Kali Society',
+    text: 'penetration-security',
   },
 }
 
